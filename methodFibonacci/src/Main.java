d012951ae3e8f49ad9098e885ee98a6c9cf290af
İlyas Keskin <ilyaskeskn8@gmail.com>
2022-11-05 21:52:46 +0300

    Methods

---
+++ methodFibonacci/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println(fib(5));
+    }
+    static int fib(int number){
+
+        if (number ==1 || number ==2){
+            return 1;
+        }
+        return fib(number -1 ) + fib(number -2);
+    }
+}
